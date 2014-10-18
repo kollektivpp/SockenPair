9322e279633d0cd12c4a01558d5db99687de6ea3
Gernot Labahn <gernot.labahn@sinnerschrader-mobile.com>
2014-10-18 15:38:49 +0200

    Make sure public is base URL

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var path = require('path'),
-	app = require('express')(),
+	express = require('express'),
+	app = express(),
 	http = require('http').Server(app),
 	colors = require('colors'),
 	io = require('socket.io')(http);
 
+// Routing
+app.use( express.static(__dirname + '/public') );
+
 app.get('/', function(req, res) {
 	// res.status(200).end();
-	res.sendFile( path.join(__dirname, 'index.html') );
+	res.sendFile( 'index.html' );
 });
 
 io.on('connection', function(socket) {
