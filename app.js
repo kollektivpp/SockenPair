beb9bfbbb1e9c8527d95bb2d605cf355278428c4
Gernot Labahn <gernot.labahn@sinnerschrader-mobile.com>
2014-10-18 15:39:20 +0200

    Simple socket communication

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
 	console.log('user connected');
+
+	socket.on('foobar', function(msg) {
+		console.log(msg);
+
+		io.emit('foobar', msg);
+	});
+
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
 });
 
 http.listen(8888, function() {
